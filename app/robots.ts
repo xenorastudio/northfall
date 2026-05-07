078d7953cc7702fa4aa6064e8e12ddd799249062
xenorastudio <swamakerswa@gmail.com>
2026-05-08 02:26:46 +0300

    SEO: use Next.js built-in sitemap.ts and robots.ts (no Netlify)

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/app?view=profile', '/app?view=settings', '/app?view=notifs', '/app?view=admin', '/app?view=edit'],
+    },
+    sitemap: 'https://northfall.blog/sitemap.xml',
+  };
+}
